568430b66672c351971c65357453d388df0c74a3
MarioBroi <inoix16@gmail.com>
2024-05-10 14:44:07 +0100

    add columns in table trains, edit readme

--- database/migrations/2024_05_10_132711_create_trains_table.php
+++ database/migrations/2024_05_10_132711_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_partenza', 100);
+            $table->string('stazione_arrivo', 100);
+            $table->time('orario_partenza', $precision = 0);
+            $table->time('orario_arrivo', $precision = 0);
+            $table->string('codice_treno', 15);
+            $table->tinyInteger('numero_carrozze');
+            $table->boolean('in_orario')->default(1)->nullable();
+            $table->boolean('cancellato')->default(1)->nullable();
             $table->timestamps();
         });
     }
