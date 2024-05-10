25095856985dc415166a4763be4921245597a418
MarioBroi <inoix16@gmail.com>
2024-05-10 15:29:58 +0100

    add classes to print more info from train database

--- resources/views/guests/trains/index.blade.php
+++ resources/views/guests/trains/index.blade.php
@@ -4,14 +4,30 @@
     <h1>Trains</h1>
     <div class="container">
         <div class="row">
-
-
             @forelse ($trains as $train)
                 <div class="col">
-                    <div class="card">
-                        Azienda: {{ $train->azienda }}
-                        Stazione Partenza: {{ $train->stazione_partenza }}
-                        Stazione Arrivo: {{ $train->stazione_arrivo }}
+                    <div class="card p-1">
+                        <span class="card-title">
+                            Azienda: {{ $train->azienda }}
+                        </span>
+                        <span class="card-text">
+                            Codice Treno: {{ $train->codice_treno }}
+                        </span>
+                        <span class="card-text">
+                            Numero carrozze: {{ $train->numero_carrozze }}
+                        </span>
+                        <span class="card-text">
+                            Stazione Partenza: {{ $train->stazione_partenza }}
+                        </span>
+                        <span class="card-text">
+                            Stazione Arrivo: {{ $train->stazione_arrivo }}
+                        </span>
+                        <span class="card-text">
+                            Orario Partenza: {{ $train->orario_partenza }}
+                        </span>
+                        <span class="card-text">
+                            Orario Arrivo: {{ $train->orario_arrivo }}
+                        </span>
                     </div>
                     <!-- /.card -->
                 </div>
